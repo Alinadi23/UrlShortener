56f91eee0a63c7d4464b6dc191b7de8e28fe3521
Kabiripour, Alireza <a.kabiripour@sep.ir>
2024-01-05 10:32:10 +0330

    Create DataAcess Layer and Related Configuration (14021015 1032)

---
+++ UrlShortener/Extensions/HostingExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.DAL.Data;
+
+namespace UrlShortener.UI.Extensions
+{
+    public static class HostingExtensions
+    {
+        private static IConfiguration _configuration { get; set; }
+
+        public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            var cnn = _configuration.GetConnectionString("Default");
+            services.AddDbContext<ApplicationDbContext>(c => c.UseInMemoryDatabase(cnn));
+            services.AddLogging(config => config.AddConfiguration(_configuration.GetSection("Logging")).AddConsole());
+
+        }
+    }
+}
--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -1,15 +1,17 @@
+using UrlShortener.UI.Extensions;
+var config = new ConfigurationBuilder().AddJsonFile("appsettings.json", true, true).Build();
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureServices(config);
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
