30d1185abfb0d8f75a66bfa37f344d4e47729aa6
Kabiripour, Alireza <a.kabiripour@sep.ir>
2024-01-05 11:53:06 +0330

    Add BL and Contract Layer (14021015 1153)

--- UrlShortener.Contracts/DTO/ShortUrlDto.cs
+++ UrlShortener.Contracts/DTO/ShortUrlDto.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UrlShortener.DAL.DTO
+namespace UrlShortener.Contracts.DTO
 {
     public class ShortUrlDTO
     {
---
+++ UrlShortener.Contracts/ServiceContracts/IShortUrlService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrlShortener.Contracts.DTO;
+
+namespace UrlShortener.Contracts.ServiceContracts
+{
+    public interface IShortUrlService
+    {
+        public ShortUrlDTO GetOriginalUrl(string path);
+
+        string GenerateShortUrl(ShortUrlDTO shortUrlDto);
+    }
+}
